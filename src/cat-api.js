441cefc0b70a3d9a44b6ddebf52f857f38cc807d
Nata2303 <121019039+Nata2303@users.noreply.github.com>
2023-06-01 19:50:38 +0400

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -10,7 +10,6 @@ export function fetchBreeds() {
       }
       return response.json();
     })
-    .then(data => data)
     .catch(error => {
       throw new Error('Failed to fetch breeds');
     });
@@ -20,24 +19,28 @@ export function fetchCatByBreed(breedId) {
   const apiKey =
     'live_3vhXWNlt8JQtPqFQ8kBfbAmwAQugtohcG5eDIzBkWliRDkaFxffdHGM5xDJWhzJm';
   const breedUrl = `https://api.thecatapi.com/v1/breeds/${breedId}?api_key=${apiKey}`;
-  const catUrl = `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${breedId}&api_key=${apiKey}`;
+  const catUrl = `https://api.thecatapi.com/v1/images/${breedId}`;
 
-  return Promise.all([fetch(breedUrl), fetch(catUrl)])
-    .then(responses => {
-      const [breedResponse, catResponse] = responses;
-
-      if (!breedResponse.ok || !catResponse.ok) {
+  return fetch(breedUrl)
+    .then(response => {
+      if (!response.ok) {
         throw new Error('Failed to fetch cat by breed');
       }
-
-      return Promise.all([breedResponse.json(), catResponse.json()]);
+      return response.json();
     })
-    .then(data => {
-      const [breedData, catData] = data;
-      const breedName = breedData[0].name || 'Unknown Breed';
-      const cat = catData[0];
-
-      return { breedName, ...cat };
+    .then(breedData => {
+      return fetch(catUrl)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Failed to fetch cat by breed');
+          }
+          return response.json();
+        })
+        .then(catData => {
+          const breedName = breedData.name || 'Unknown Breed';
+          const cat = catData[0];
+          return { breedName, ...cat };
+        });
     })
     .catch(error => {
       throw new Error('Failed to fetch cat by breed');
